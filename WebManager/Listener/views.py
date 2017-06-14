be531f1d660a9f623f0f0d004a8c59b0e50ab401
icancode23 <ernipunarora@gmail.com>
2017-06-14 13:21:56 +0530

    Started Project

---
+++ WebManager/Listener/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+# Create your views here.
+# Listens to errors in apache log for stacks that use apache as a virtual host
+
+def listenErrorApache(request):
+	a=request.GET.get("test")
+	print a
+	return HttpResponse("Received OK")
+
+
